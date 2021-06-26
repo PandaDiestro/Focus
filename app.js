ace61b7ff8a51e8f948cb63248c15f17ef590fcd
Panda Diestro <71461113+PandaDiestro@users.noreply.github.com>
2021-06-26 11:11:32 -0500

    main script

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ focusarray.forEach(element => {
 				headcall.innerHTML = '<title>CONCENTRATE</title><link rel="icon" href="https://1.bp.blogspot.com/-8ZU4kgImyJQ/YMI9huzZK5I/AAAAAAAABM8/e9z_hwNDzeYp5aUKrDNBU5PAqxuRYgB_gCLcBGAsYHQ/s0/focus.png">'; 
 				bodycall.style.backgroundColor = 'black';
 				bodycall.innerHTML = '<br><br><br><div onselectstart = "return false" style = "cursor: default; color: white; font-family: calibri; font-size: 50px; text-align: center;">CONCENTRATE<b>!</b></div><br><br><div style = "text-align: center;"><img alt = "no image ya dumbnuts" src = "https://lh3.googleusercontent.com/-rSxqjH019RU/YKBG3FaV4FI/AAAAAAAABK0/K_VuUIg3s9YO-pBihQdcIzDqF_H1XRWMACLcBGAsYHQ/focus%2B%25281%2529.png" draggable = "false">';
-			},499);
+			},500);
 		}
 	}
 });
