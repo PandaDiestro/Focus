62a00b76892035b62158818704355741b51c3493
Panda Diestro <71461113+PandaDiestro@users.noreply.github.com>
2021-07-07 09:34:42 -0500

    main script
    Turns out that the reddit bug was stil there at reddit.com but not at subreddits which is strange af, while i try to fix it, the interval should do its job.

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ focusarray.forEach(element => {
 	{
 		window.onload = function()
 		{
-			for (i = htmlAttributesCall.length; i >= 0; i -= 1)
-			{
-				htmlcall.removeAttribute(htmlAttributesCall[i])
-			}
-			headcall.innerHTML = '<title>CONCENTRATE</title><link rel="icon" href="https://1.bp.blogspot.com/-8ZU4kgImyJQ/YMI9huzZK5I/AAAAAAAABM8/e9z_hwNDzeYp5aUKrDNBU5PAqxuRYgB_gCLcBGAsYHQ/s0/focus.png">'; 
-			bodycall.style.backgroundColor = 'black';
-			bodycall.innerHTML = '<br><br><br><div onselectstart = "return false" style = "cursor: default; color: white; font-family: calibri; font-size: 50px; text-align: center;">CONCENTRATE<b>!</b></div><br><br><div style = "text-align: center;"><img alt = "no image ya dumbnuts" src = "https://lh3.googleusercontent.com/-rSxqjH019RU/YKBG3FaV4FI/AAAAAAAABK0/K_VuUIg3s9YO-pBihQdcIzDqF_H1XRWMACLcBGAsYHQ/focus%2B%25281%2529.png" draggable = "false">';
-		
+			setInterval(function(){
+				for (i = htmlAttributesCall.length; i >= 0; i -= 1)
+				{
+					htmlcall.removeAttribute(htmlAttributesCall[i])
+				}
+				headcall.innerHTML = '<title>CONCENTRATE</title><link rel="icon" href="https://1.bp.blogspot.com/-8ZU4kgImyJQ/YMI9huzZK5I/AAAAAAAABM8/e9z_hwNDzeYp5aUKrDNBU5PAqxuRYgB_gCLcBGAsYHQ/s0/focus.png">'; 
+				bodycall.style.backgroundColor = 'black';
+				bodycall.innerHTML = '<br><br><br><div onselectstart = "return false" style = "cursor: default; color: white; font-family: calibri; font-size: 50px; text-align: center;">CONCENTRATE<b>!</b></div><br><br><div style = "text-align: center;"><img alt = "no image ya dumbnuts" src = "https://lh3.googleusercontent.com/-rSxqjH019RU/YKBG3FaV4FI/AAAAAAAABK0/K_VuUIg3s9YO-pBihQdcIzDqF_H1XRWMACLcBGAsYHQ/focus%2B%25281%2529.png" draggable = "false">';
+			
+			}, 600);
+			
 			alert("FOCUS!");
 		}
 	}
