d9ea52dbdaa652b4f4d25e2af92addd52b6f9779
Panda Diestro <71461113+PandaDiestro@users.noreply.github.com>
2021-08-19 16:56:44 -0500

    main script
    So I changed the whole concept, now instead of modifying the html of the distraction, it just redirects you to a "focus page", so now it works at everywhere and its faster and less memory consuming, now i just have to add the rest button and it will be finished. :DDDD

--- app.js
+++ app.js
@@ -3,36 +3,16 @@ const bodycall = document.getElementsByTagName('body')[0];
 const headcall = document.getElementsByTagName('head')[0];
 const htmlcall = document.getElementsByTagName('html')[0];
 let htmlAttributesCall = htmlcall.getAttributeNames();
+let bodyAttributesCall = bodycall.getAttributeNames();
 var xhr = new XMLHttpRequest();
-
-function go(){
-	xhr.open('GET', 'https://raw.githubusercontent.com/PandaDiestro/Focus/main/icons/focus.ico', true);
-	xhr.responseType = 'blob';
-	xhr.onload = function(e) {
-		var lnk = document.createElement('link');
-		lnk.rel = 'icon';
-		lnk.href = window.URL.createObjectURL(this.response);
-		document.head.appendChild(lnk);
-	};
-	xhr.send();
-}
+let yes;
 
 focusarray.forEach(element => {
 	if(window.location.href.search(element) != -1){
+		
 		window.onload = function(){
-			setInterval(function(){
-				for (i = htmlAttributesCall.length; i >= 0; i -= 1){
-					htmlcall.removeAttribute(htmlAttributesCall[i])
-				}
-				headcall.innerHTML = '<title>CONCENTRATE</title>';
-				go();
-				bodycall.style.backgroundColor = 'black';
-				bodycall.innerHTML = '<br><br><br><div onselectstart = "return false" style = "cursor: default; color: white; font-family: calibri; font-size: 50px; text-align: center;">CONCENTRATE<b>!</b></div><br><br><div style = "text-align: center;"><img alt = "no image ya dumbnuts" src = "https://raw.githubusercontent.com/PandaDiestro/Focus/main/focus-clean.png" draggable = "false">';
-				
-			}, 600);
-			
-			
 			alert("close this tab you procastinating dumbass");
+			location.href = "https://pandadiestro.github.io/focus-page/";
 		}
 	}
 });
