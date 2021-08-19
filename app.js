fd9916e8c68b8ef5c9062106f156aba4f357a362
Panda Diestro <71461113+PandaDiestro@users.noreply.github.com>
2021-08-19 16:57:28 -0500

    main script
    forgot cleaning the unused variables

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 const focusarray = ['facebook', 'youtube', 'twitter', 'discord', 'messenger', 'instagram', 'reddit', 'fandom', 'meme', 'shitpost', 'twitch'];
-const bodycall = document.getElementsByTagName('body')[0];
-const headcall = document.getElementsByTagName('head')[0];
-const htmlcall = document.getElementsByTagName('html')[0];
-let htmlAttributesCall = htmlcall.getAttributeNames();
-let bodyAttributesCall = bodycall.getAttributeNames();
-var xhr = new XMLHttpRequest();
-let yes;
 
 focusarray.forEach(element => {
 	if(window.location.href.search(element) != -1){
