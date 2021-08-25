b5480614e1fe3ee1ade52424e550695a0ec36821
Panda Diestro <71461113+PandaDiestro@users.noreply.github.com>
2021-08-25 08:53:34 -0500

    main script

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const focusarray = ['facebook', 'youtube', 'twitter', 'discord', 'messenger', 'i
 focusarray.forEach(element => {
 	if(window.location.href.search(element) != -1){
 		
-		window.onload = function(){
-			alert("close this tab you procastinating dumbass");
-			location.href = "https://pandadiestro.github.io/focus-page/";
-		}
+		alert("close this tab you procastinating dumbass");
+		location.href = "https://pandadiestro.github.io/focus-page/";
 	}
 });
